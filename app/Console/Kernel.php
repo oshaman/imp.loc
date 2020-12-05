b5cb4f43eddd8496ecc2f83da2ee4cb98f695e85
oshaman <reg_forall@bigmir.net>
2020-12-05 09:10:43 +0200

    0.2

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Console;
 
 use App\Moyo;
+use App\Product;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use Illuminate\Support\Facades\DB;
@@ -33,14 +34,15 @@ class Kernel extends ConsoleKernel
                 set_time_limit(600);
                 $moyo = new Moyo();
                 $data = $moyo->getData();
-                $cats = $moyo->getCategories($data);
-                DB::table('categories')->insertOrIgnore($cats);
+//                $cats = $moyo->getCategories($data);
+//                DB::table('categories')->insertOrIgnore($cats);
 
+                Product::query()->truncate();
                 $offers = $moyo->getOffers($data);
                 foreach (array_chunk($offers, 3000) as $value) {
                     DB::table('products')->insertOrIgnore($value);
                 }
-            })->hourly();
+            })->dailyAt('01:00');
         } catch (Exception $exceptione) {
             Log::info('Ошибка ======> ' . $exceptione);
         }
